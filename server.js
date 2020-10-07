19d0ce129d73c4303c53e01e6c587e608638202a
Salomon Sada <sajosaca@gmail.com>
2020-10-07 17:36:58 -0300

    User model, express validator, middleware to body

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 // Conect Database
 connectDB();
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("API runing"));
 
@@ -12,7 +14,7 @@ app.get("/", (req, res) => res.send("API runing"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/posts", require("./routes/api/posts")); 
+app.use("/api/posts", require("./routes/api/posts"));
 // Port listener
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
