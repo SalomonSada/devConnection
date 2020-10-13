62af5105a1d312b6a5e743964be041aa23d47144
Salomon Sada <sajosaca@gmail.com>
2020-10-13 16:13:54 -0300

    delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -165,7 +165,7 @@ router.put(
   [
     auth,
     [
-      check('title', 'Tittle is required').not().isEmpty(),
+      check('title', 'Title is required').not().isEmpty(),
       check('company', 'Company is required').not().isEmpty(),
       check('from', 'From date is required').not().isEmpty(),
     ],
@@ -209,5 +209,28 @@ router.put(
     }
   }
 );
+
+// @route   DELETE api/profile/expirience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
