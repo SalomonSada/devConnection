aaf20b780d7b2e02ee75d134ece9e27114ebde66
Salomon Sada <sajosaca@gmail.com>
2020-10-17 12:41:40 -0300

    Landing, Login, Register (just react)

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,57 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Succes');
+  };
+
+  return (
+    <Fragment>
+      {/* Alert! :    <div className="alert alert-danger">Invalid Credentials</div> */}
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user m-1"></i>Sign into your account
+      </p>
+      <form action="dashboard.html" className="form">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign up</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
